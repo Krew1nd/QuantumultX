ff5eac0822d643390d7b409a654c5d236fdc2a08
Krewind <87192031+Krewind@users.noreply.github.com>
2021-08-08 11:00:46 +0800

    Update and rename IPwhois.js to IP-Whois.js

--- IP-Whois.js
+++ IP-Whois.js
@@ -7,7 +7,7 @@ var obj = JSON.parse(body);
 var title = obj['country'];
 var subtitle = obj['isp'];
 var ip = obj['ip'];
-var description = "国家" + ":" + obj['country'] + '\n' + "城市" + ":" + obj['city'] + '\n' + "运营商" + ":" + obj['isp'] + '\n' + "IP" + ":" + obj['ip'];
+var description = "国家/地区" + ":" + obj['country'] + '\n' + "城市" + ":" + obj['city'] + '\n' + "ISP" + ":" + obj['isp'] + '\n' + "IP" + ":" + obj['ip'];
 
 
 $done({title, subtitle, ip, description});
