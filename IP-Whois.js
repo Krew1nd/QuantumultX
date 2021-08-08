b77d0de87e9548f9de8b664fd283f6794b75fa50
Krewind <87192031+Krewind@users.noreply.github.com>
2021-08-08 11:28:29 +0800

    Update IP-Whois.js

--- IP-Whois.js
+++ IP-Whois.js
@@ -14,9 +14,9 @@ function ISP_ValidCheck(para) {
 var body = $response.body;
 var obj = JSON.parse(body);
 var title = obj['country'];
-var subtitle = obj['isp'];
+var subtitle = ISP_ValidCheck(obj['isp']);
 var ip = obj['ip'];
-var description = "国家/地区" + ":" + obj['country'] + '\n' + "城市" + ":" + obj['city'] + '\n' + "ISP" + ":" + obj['isp'] + '\n' + "IP" + ":" + obj['ip'];
+var description = "国家/地区" + ":" + obj['country'] + '\n' + "城市" + ":" + obj['city'] + '\n' + "ISP" + ":" + ISP_ValidCheck(obj['isp']) + '\n' + "IP" + ":" + obj['ip'];
 
 
 $done({title, subtitle, ip, description});
