5487a07728d788fe5a6c3c4e094c0485c7af906e
Krewind <87192031+Krewind@users.noreply.github.com>
2021-08-08 16:25:03 +0800

    Update IP-API.js

--- IP-API.js
+++ IP-API.js
@@ -2,12 +2,20 @@ if ($response.statusCode != 200) {
   $done(Null);
 }
 
+function City(para) {
+  if(para =="Central"){
+  return "中环"
+  } else
+  {
+  return para
+  }
+}
+
 var body = $response.body;
 var obj = JSON.parse(body);
 var title = obj['country'];
 var subtitle = obj['isp'];
 var ip = obj['query'];
-var description = "国家/地区" + ":" + obj['country'] + '\n' + "城市" + ":" + obj['city'] + '\n' + "运营商" + ":" + obj['isp'] + '\n' + "数据中心" + ":" + obj['org'] + '\n' + "IP" + ":" + obj['query'];
-
+var description = "国家/地区" + ":" + obj['country'] + '\n' + "城市" + ":" + City(obj['isp']) + '\n' + "运营商" + ":" + obj['isp'] + '\n' + "数据中心" + ":" + obj['org'] + '\n' + "IP" + ":" + obj['query'];
 
 $done({title, subtitle, ip, description});
