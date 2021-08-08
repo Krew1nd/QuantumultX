64b139676e7912e9f475f146cea88076073d24ca
Krewind <87192031+Krewind@users.noreply.github.com>
2021-08-08 11:46:38 +0800

    Update IP-Whois.js

--- IP-Whois.js
+++ IP-Whois.js
@@ -2,9 +2,9 @@ if ($response.statusCode != 200) {
   $done(Null);
 }
 
-function ISP_ValidCheck(para) {
+function ISP(para) {
   if(para =="Data Communication Business Group"){
-  return "Chunghwa Telecom"
+  return "Chunghwa Telecom Co., Ltd."
   } else
   {
   return para
@@ -14,9 +14,9 @@ function ISP_ValidCheck(para) {
 var body = $response.body;
 var obj = JSON.parse(body);
 var title = obj['country'];
-var subtitle = ISP_ValidCheck(obj['isp']);
+var subtitle = ISP(obj['isp']);
 var ip = obj['ip'];
-var description = "国家/地区" + ":" + obj['country'] + '\n' + "城市" + ":" + obj['city'] + '\n' + "ISP" + ":" + ISP_ValidCheck(obj['isp']) + '\n' + "IP" + ":" + obj['ip'];
+var description = "位置" + ":" + obj['country'] + '\n' + "ISP" + ":" + ISP(obj['isp']) + '\n' + "IP" + ":" + obj['ip'];
 
 
 $done({title, subtitle, ip, description});
