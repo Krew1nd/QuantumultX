e44bcb7f57b9556b7402faf34e3ad5a9ae5750ba
Krewind <87192031+Krewind@users.noreply.github.com>
2021-08-08 16:25:54 +0800

    Update IP-API.js

--- IP-API.js
+++ IP-API.js
@@ -16,6 +16,6 @@ var obj = JSON.parse(body);
 var title = obj['country'];
 var subtitle = obj['isp'];
 var ip = obj['query'];
-var description = "国家/地区" + ":" + obj['country'] + '\n' + "城市" + ":" + City(obj['isp']) + '\n' + "运营商" + ":" + obj['isp'] + '\n' + "数据中心" + ":" + obj['org'] + '\n' + "IP" + ":" + obj['query'];
+var description = "国家/地区" + ":" + obj['country'] + '\n' + "城市" + ":" + City(obj['isp']) + '\n' + "服务商" + ":" + obj['isp'] + '\n' + "数据中心" + ":" + obj['org'] + '\n' + "IP" + ":" + obj['query'];
 
 $done({title, subtitle, ip, description});
