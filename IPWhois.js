053f71ac2ba4ac396ef7ac0a702d9f09d1ebfc58
Krewind <87192031+Krewind@users.noreply.github.com>
2021-08-08 17:05:57 +0800

    Update IPWhois.js

--- IPWhois.js
+++ IPWhois.js
@@ -2,6 +2,15 @@ if ($response.statusCode != 200) {
   $done(Null);
 }
 
+function Country(para) {
+  if(para =="大韩民国"){
+  return "韩国"
+  } else
+  {
+  return para
+  }
+}
+
 function ISP(para) {
   if(para =="Data Communication Business Group"){
   return "Chunghwa Telecom Co., Ltd."
@@ -13,10 +22,10 @@ function ISP(para) {
 
 var body = $response.body;
 var obj = JSON.parse(body);
-var title = obj['country'];
+var title = Country(obj['country']);
 var subtitle = ISP(obj['isp']);
 var ip = obj['ip'];
-var description = "Location" + ":" + obj['country'] + '\n' + "ISP" + ":" + ISP(obj['isp']) + '\n' + "IP" + ":" + obj['ip'];
+var description = "Location" + ":" + Country(obj['country']) + '\n' + "ISP" + ":" + ISP(obj['isp']) + '\n' + "IP" + ":" + obj['ip'];
 
 
 $done({title, subtitle, ip, description});
