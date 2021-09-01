d944d32ae6b166bdd1fffbdbab01a22ca55912fc
Krew1nd <87192031+Krew1nd@users.noreply.github.com>
2021-09-01 23:28:16 +0800

    Update jd_plantBean.js

--- Task/jd_plantBean.js
+++ Task/jd_plantBean.js
@@ -30,11 +30,7 @@ let jdNotify = true;//是否开启静默运行。默认true开启
 let cookiesArr = [], cookie = '', jdPlantBeanShareArr = [], isBox = false, notify, newShareCodes, option, message, subTitle;
 //京东接口地址
 const JD_API_HOST = 'https://api.m.jd.com/client.action';
-//助力好友分享码(最多3个,否则后面的助力失败)
-//此此内容是IOS用户下载脚本到本地使用，填写互助码的地方，同一京东账号的好友互助码请使用@符号隔开。
-//填写示例（iOS只支持2个京东账号）
-//好友shareCode,不同好友的shareCode中间用@符号隔开
-let shareCodes = [''] // IOS本地脚本用户这个列表填入你要助力的好友的shareCode
+let shareCodes = [''];
 let allMessage = ``;
 let currentRoundId = null;//本期活动id
 let lastRoundId = null;//上期id
