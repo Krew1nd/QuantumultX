b39a112c9b19148104c9cd9c7ca11093ee3d5e3b
Krew1nd <87192031+Krew1nd@users.noreply.github.com>
2022-03-23 01:31:01 +0800

    Update IP-API.js

--- IP-API.js
+++ IP-API.js
@@ -5,7 +5,7 @@ if ($response.statusCode != 200) {
 var body = $response.body;
 var obj = JSON.parse(body);
 var title = obj['country'];
-var subtitle = obj['isp'];
+var subtitle = obj['as'];
 var ip = obj['query'];
 var description = "国家/地区" + ":" + obj['country'] + '\n' + "城市" + ":" + obj['city'] + '\n' + "ISP" + ":" + obj['isp'] + '\n' + "AS" + ":" + obj['as'] + '\n' + "IP" + ":" + obj['query'];
 
