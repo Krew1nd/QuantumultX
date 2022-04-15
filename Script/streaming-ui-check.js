3295477ff8ed086b168d953bfa6e97372d0a5db3
Krew1nd <87192031+Krew1nd@users.noreply.github.com>
2022-04-15 19:23:52 +0800

    Update streaming-ui-check.js

--- Script/streaming-ui-check.js
+++ Script/streaming-ui-check.js
@@ -96,7 +96,7 @@ const message = {
   }
 
   let content = "------------------------------"+"</br>"+([result["YouTube"],result["Netflix"],result["Disney"],result["Dazn"],result["Paramount"],result["Discovery"]]).join("</br></br>")
-  content = content + "</br>------------------------------</br>"+"<font color=#CD5C5C >"+"<b>节点</b> ➤ " + $environment.params+ "</font>"
+  content = content + "</br>------------------------------</br>"+"<font color=#CD5C5C >"+ $environment.params+ "</font>"
   content =`<p style="text-align: center; font-family: -apple-system; font-size: large; font-weight: thin">` + content + `</p>`
 //  cnt = `<p style="text-align: center; font-family: -apple-system; font-size: large; font-weight: thin">` +'----------------------</br></br>'+result["Disney"]+'</br></br>----------------------</br>'+$environment.params + `</p>`
 $configuration.sendMessage(message).then(resolve => {
@@ -107,7 +107,7 @@ $configuration.sendMessage(message).then(resolve => {
     if (resolve.ret) {
       let output=JSON.stringify(resolve.ret[message.content])? JSON.stringify(resolve.ret[message.content]).replace(/\"|\[|\]/g,"").replace(/\,/g," ➤ ") : $environment.params
       let content = "--------------------------------------</br>"+([result["Dazn"],result["Discovery"],result["Paramount"],result["Disney"],result["Netflix"],result["YouTube"]]).join("</br></br>")
-      content = content + "</br>--------------------------------------</br>"+"<font color=#CD5C5C>"+"<b>节点</b> ➤ " + output+ "</font>"
+      content = content + "</br>--------------------------------------</br>"+"<font color=#CD5C5C>"+ output+ "</font>"
       content =`<p style="text-align: center; font-family: -apple-system; font-size: large; font-weight: thin">` + content + `</p>`
       //$notify(typeof(output),output)
       console.log(output);
