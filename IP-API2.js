0ffc444686996b23d7d1af07a9d1bd2bb68593a1
Krew1nd <87192031+Krew1nd@users.noreply.github.com>
2022-06-22 23:14:05 +0800

    Update IP-API2.js

--- IP-API2.js
+++ IP-API2.js
@@ -5,7 +5,7 @@ if ($response.statusCode != 200) {
 var body = $response.body;
 var obj = JSON.parse(body);
 var title = "Quantumult X";
-var subtitle = "NETWORK TOOL FOR KREW1ND";
+var subtitle = obj['country'] + "•" + obj['isp'];
 var ip = obj['query'];
 var description = "国家/地区" + ":" + obj['country'] + '\n' + "城市" + ":" + obj['city'] + '\n' + "ISP" + ":" + obj['isp'] + '\n' + "AS" + ":" + obj['as'] + '\n' + "IP" + ":" + obj['query'];
 
